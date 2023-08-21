480c18c6cb8079809f768d3dde083e29a3f3c106
kfirsandberg <kfirsan07@gmail.com>
2023-08-21 15:31:33 +0300

    final project no bonus

--- BasePage.py
+++ BasePage.py
@@ -1,77 +1,61 @@
 import time
-from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import allure
-from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
-
-class BasePage():
+class Buy_me_site():
     def __init__(self, driver):
         self.driver = driver
-    def screenshot(self,name):
-        allure.attach(self.driver.get_screenshot_as_png(), name=name, attachment_type=allure.attachment_type.PNG)
-        self.my_screenshots += 1
     def sign_page(self):
-        try:
-            self.driver.implicitly_wait(10)
-            self.driver.find_element(By.CLASS_NAME, value='notSigned').click()
-            self.driver.find_element(By.XPATH, '//span[@aria-label="להרשמה"]').click()
-            self.driver.find_element(By.ID, 'ember1921').send_keys('כפיר')
-            self.driver.find_element(By.ID, 'ember1928').send_keys('kfir@gmail.com')
-            self.driver.find_element(By.ID, 'valPass').send_keys('Kfir1234')
-            self.driver.find_element(By.ID, 'ember1938').send_keys('Kfir1234')
-            self.driver.find_element(By.CLASS_NAME, 'fill').click()
-            self.driver.find_element(By.ID, 'ember1942').click()
-        except NoSuchElementException:
-            self.screenshot(f"Element_not_found{self.my_screenshots}")
-
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.CLASS_NAME, value='notSigned').click()
+        self.driver.find_element(By.XPATH, '//span[@aria-label="להרשמה"]').click()
+        self.driver.find_element(By.ID, 'ember1921').send_keys('כפיר')
+        self.driver.find_element(By.ID, 'ember1928').send_keys('kfir@gmail.com')
+        self.driver.find_element(By.ID, 'valPass').send_keys('Kfir1234')
+        self.driver.find_element(By.ID, 'ember1938').send_keys('Kfir1234')
+        self.driver.find_element(By.CLASS_NAME, 'fill').click()
+        self.driver.find_element(By.ID, 'ember1942').click()
     def assert_name(self):
         element = self.driver.find_element(By.ID, value="ember1917")
         name_text = element.get_attribute('value')
         assert name_text == 'כפיר'
-    def choose(self):
-        try:
-            self.driver.set_page_load_timeout(10)
-            self.driver.implicitly_wait(10)
-            self.driver.find_element(By.XPATH, '//span[@title="סכום"]').click()
-            time.sleep(1)
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1075'))).click()
-            self.driver.find_element(By.XPATH, '//span[@title="אזור"]').click()
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1110'))).click()
-            self.driver.find_element(By.XPATH, '//span[@title="קטגוריה"]').click()
-            time.sleep(1)
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1172'))).click()
-            self.driver.find_element(By.ID,value='ember1199').click()
-            self.driver.set_page_load_timeout(10)
-            time.sleep(1)
-            assert self.driver.current_url == 'https://buyme.co.il/search?budget=1&category=419&region=11'
-            self.driver.find_element(By.LINK_TEXT,value='BUYME CHEF - מגוון מסעדות שף').click()
-            self.driver.find_element(By.XPATH ,value="//input[@data-parsley-max='1500']").send_keys('100')
-            self.driver.find_element(By.XPATH ,value="//input[@data-parsley-max='1500']").send_keys(Keys.ENTER)
-        except NoSuchElementException:
-            self.screenshot(f"Element_not_found{self.my_screenshots}")
+    def choose_gift(self):
+        self.driver.set_page_load_timeout(10)
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH, '//span[@title="סכום"]').click()
+        time.sleep(1)
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1075'))).click()
+        self.driver.find_element(By.XPATH, '//span[@title="אזור"]').click()
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1110'))).click()
+        self.driver.find_element(By.XPATH, '//span[@title="קטגוריה"]').click()
+        time.sleep(1)
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.ID,'ember1172'))).click()
+        self.driver.find_element(By.ID,value='ember1199').click()
 
-    def send(self):
-        try:
-            self.driver.implicitly_wait(10)
-            self.driver.find_element(By.XPATH,value="//input[@class='ember-view ember-text-field']").send_keys('kfir')
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME,'selected-text'))).click()
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.XPATH,
-                                                                                  '//li[@class="ember-view bm-select-option"]'))).click()
-            WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME,'textarea-clear-all'))).click()
-            self.driver.find_element(By.CLASS_NAME,value="parsley-success").send_keys('מזל  טוב שותף')
-            image_path = '/Users/kfirzand/Downloads/macabbi-haifa.jpeg'
-            self.driver.find_element(By.XPATH, value='//input[@type="file"]').send_keys(image_path)
-            time.sleep(2)
-            self.driver.find_element(By.XPATH, "//span[@class='label']").click()
-            icon_list = self.driver.find_elements(By.CLASS_NAME, value='method-icon')
-            icon_list[1].click()
-            self.driver.find_element(By.ID,value='email').send_keys('kfir@gmail.com')
-            self.driver.find_element(By.XPATH,value="//input[@placeholder='שם שולח המתנה']").send_keys('kfir')
-            element2 = self.driver.find_element(By.XPATH,value="//input[@placeholder='שם שולח המתנה']")
-            name = element2.get_attribute('value')
-            assert name == 'kfir'
-        except NoSuchElementException:
-            self.screenshot(f"Element_not_found{self.my_screenshots}")
+        self.driver.set_page_load_timeout(10)
+        time.sleep(1)
+        assert self.driver.current_url == 'https://buyme.co.il/search?budget=1&category=419&region=11'
+        self.driver.find_element(By.LINK_TEXT,value='BUYME CHEF - מגוון מסעדות שף').click()
+        self.driver.find_element(By.XPATH ,value="//input[@data-parsley-max='1500']").send_keys('100')
+        self.driver.find_element(By.XPATH ,value="//input[@data-parsley-max='1500']").send_keys(Keys.ENTER)
+    def send_info(self):
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH,value="//input[@class='ember-view ember-text-field']").send_keys('kfir')
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME,'selected-text'))).click()
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located((By.XPATH,
+                                               '//li[@class="ember-view bm-select-option"]'))).click()
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(
+            (By.CLASS_NAME,'textarea-clear-all'))).click()
+        self.driver.find_element(By.CLASS_NAME,value="parsley-success").send_keys('מזל  טוב שותף')
+        image_path = '/Users/kfirzand/Downloads/macabbi-haifa.jpeg'
+        self.driver.find_element(By.XPATH, value='//input[@type="file"]').send_keys(image_path)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//span[@class='label']").click()
+        icon_list = self.driver.find_elements(By.CLASS_NAME, value='method-icon')
+        icon_list[0].click()
+        self.driver.find_element(By.ID,value='email').send_keys('kfir@gmail.com')
+        self.driver.find_element(By.XPATH,value="//input[@placeholder='שם שולח המתנה']").send_keys('kfir')
+        element2 = self.driver.find_element(By.XPATH,value="//input[@placeholder='שם שולח המתנה']")
+        name = element2.get_attribute('value')
+        assert name == 'kfir'
--- tests_Buyme.py
+++ tests_Buyme.py
@@ -1,28 +1,38 @@
 import unittest
 from selenium import webdriver
-from BasePage import BasePage
+from BasePage import Buy_me_site
+import allure
+from allure_commons.types import AttachmentType
 import json
 def json_setup():
-    json_file = open('Json_Setup.json', 'r')
-    data = json.load(json_file)
+    with open("Json_Setup.json")as json_file:
+       data = json.load(json_file)
     return data
-
+def screenshot(self):
+    allure.attach(name="nosuchlementScreenshot",
+                  body=self.driver.get_screenshot_as_png(), attachment_type=AttachmentType.PNG)
 class TestSignUp(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         data =json_setup()
         self.driver.get(data['url'])
-        # self.sign_up_page = BasePage(self.driver)
     def test_sign_in(self):
-        BasePage.sign_page(self)
-        BasePage.assert_name(self)
-
-    def test_chosse(self):
-        BasePage.choose(self)
-    def test_send(self):
-        data =json_setup()
-        self.driver.get(data['url2'])
-        BasePage.send(self)
-
+        try:
+            Buy_me_site.sign_page(self)
+            Buy_me_site.assert_name(self)
+        except :
+            screenshot(self)
+    def test_chosse_gift(self):
+        try:
+            Buy_me_site.choose_gift(self)
+        except :
+            screenshot(self)
+    def test_send_info(self):
+        try:
+            data =json_setup()
+            self.driver.get(data['url2'])
+            Buy_me_site.send_info(self)
+        except :
+            screenshot(self)
     def tearDown(self):
         self.driver.quit()
